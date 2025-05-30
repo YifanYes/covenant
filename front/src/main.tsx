b1dd4791fc99186bf530044dd983e7d94f151555
Yifan <yifanyemontpe@gmail.com>
2025-05-30 18:33:55 +0200

    add react router

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,15 +1,15 @@
 import { QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './i18n.ts'
 import './index.css'
+import { Router } from './Router.tsx'
 import { queryClient } from './utils/trpc.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <Router />
     </QueryClientProvider>
   </StrictMode>
 )
