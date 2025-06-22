0fd74486f4c235d3f78c6ece7165a72cd720d96f
Yifan Ye <30553455+YifanYes@users.noreply.github.com>
2025-06-22 16:22:15 +0200

    feat: add sidebar and card components

--- front/src/views/Dashboard.tsx
+++ front/src/views/Dashboard.tsx
@@ -1,13 +1,24 @@
+import ObjectiveCard from '@/components/ObjectiveCard'
 import { trpc } from '@/utils/trpc'
 import { useQuery } from '@tanstack/react-query'
 
 export default function Dashboard() {
   const { data, isLoading } = useQuery(trpc.objectives.getAll.queryOptions())
 
-  console.log(data)
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div>
       <h1>Dashboard</h1>
+      {data?.objectives?.map((objective) => (
+        <ObjectiveCard
+          key={objective.id}
+          name={objective.name}
+          {...(objective.description && { description: objective.description })}
+        />
+      ))}
     </div>
   )
 }
