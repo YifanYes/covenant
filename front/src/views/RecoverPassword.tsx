0f8b854acde2f39c5216fae3c883bcf18a15adfb
Denis Gudiña Nuñez <syrewolfdigital@gmail.com>
2025-07-20 20:15:28 +0200

    feat(recover password): add view

--- front/src/views/RecoverPassword.tsx
+++ front/src/views/RecoverPassword.tsx
@@ -1,7 +1,59 @@
-export default function RecoverPassword() {
+import LoaderButton from '@/components/LoaderButton'
+import PasswordInput from '@/components/PasswordInput'
+import { useSnackbar } from '@/hooks/useSnackbar'
+import { trpc } from '@/utils/trpc'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from '@tanstack/react-query'
+import { type FC } from 'react'
+import { useForm } from 'react-hook-form'
+import { useTranslation } from 'react-i18next'
+import { useNavigate } from 'react-router'
+import { recoverPasswordSchema } from '../../../server/schemas/auth.schemas'
+
+export const RecoverPassword: FC = () => {
+  const { t } = useTranslation()
+  const { show } = useSnackbar()
+  const navigate = useNavigate()
+
+  const recoverPasswordMutation = useMutation(
+    trpc.auth.recoverPassword.mutationOptions({
+      onSuccess: (data) => {
+        show({
+          variant: 'default',
+          title: t('recover_password.success.title')
+        })
+        navigate('/login', { state: { email: data.user.email } })
+      },
+      onError: (error) => {
+        console.log(error)
+        show({
+          variant: 'destructive',
+          title: t('recover_password.error.title')
+        })
+      }
+    })
+  )
+
+  const { register, handleSubmit, watch } = useForm({ resolver: zodResolver(recoverPasswordSchema) })
+
+  const password = watch('password')
+  const confirmPassword = watch('confirmPassword')
+
+  const onSubmit = handleSubmit((data) => recoverPasswordMutation.mutate(data))
+
   return (
-    <div>
-      <h1>Recover Password</h1>
+    <div className='w-xs flex flex-col gap-2.5'>
+      <h2>{t('recover_password.title')}</h2>
+      <PasswordInput placeholder={t('recover_password.password')} {...register('password')} />
+      <PasswordInput placeholder={t('recover_password.confirm_password')} {...register('confirmPassword')} />
+      <LoaderButton
+        disabled={!password || !confirmPassword || password !== confirmPassword}
+        isLoading={recoverPasswordMutation.isPending}
+        label={t('recover_password.button')}
+        onClick={onSubmit}
+      />
     </div>
   )
 }
+
+export default RecoverPassword
