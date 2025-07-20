3a274b32d0fce934e90d679d9cf027e9611bfb8a
Denis Gudiña Nuñez <syrewolfdigital@gmail.com>
2025-07-20 20:14:46 +0200

    feat(forgot password): add schemas

--- server/schemas/auth.schemas.ts
+++ server/schemas/auth.schemas.ts
@@ -1,5 +1,10 @@
 import { z } from 'zod/v4'
 
+export const forgotPasswordSchema = z.object({
+  email: z.email()
+})
+export type ForgotPasswordSchema = z.infer<typeof forgotPasswordSchema>
+
 export const signUpSchema = z.object({
   email: z.email(),
   password: z.string(),
