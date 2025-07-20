bd62d8f166d0c4a5c4a48521ae812bad34abb85a
Denis Gudiña Nuñez <syrewolfdigital@gmail.com>
2025-07-20 21:05:24 +0200

    fix(recover password): add error control + replace service

--- front/src/views/RecoverPassword.tsx
+++ front/src/views/RecoverPassword.tsx
@@ -1,10 +1,11 @@
 import LoaderButton from '@/components/LoaderButton'
 import PasswordInput from '@/components/PasswordInput'
+import useHashParams from '@/hooks/use-hash-params'
 import { useSnackbar } from '@/hooks/useSnackbar'
 import { trpc } from '@/utils/trpc'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useMutation } from '@tanstack/react-query'
-import { type FC } from 'react'
+import { useEffect, type FC } from 'react'
 import { useForm } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router'
@@ -14,15 +15,20 @@ export const RecoverPassword: FC = () => {
   const { t } = useTranslation()
   const { show } = useSnackbar()
   const navigate = useNavigate()
+  const hashParams = useHashParams()
+
+  const accessToken = hashParams.access_token
+  const refreshToken = hashParams.refresh_token
+  const errorCode = hashParams.error_code
 
   const recoverPasswordMutation = useMutation(
-    trpc.auth.recoverPassword.mutationOptions({
-      onSuccess: (data) => {
+    trpc.auth.updatePassword.mutationOptions({
+      onSuccess: () => {
         show({
           variant: 'default',
           title: t('recover_password.success.title')
         })
-        navigate('/login', { state: { email: data.user.email } })
+        navigate('/login')
       },
       onError: (error) => {
         console.log(error)
@@ -39,7 +45,13 @@ export const RecoverPassword: FC = () => {
   const password = watch('password')
   const confirmPassword = watch('confirmPassword')
 
-  const onSubmit = handleSubmit((data) => recoverPasswordMutation.mutate(data))
+  const onSubmit = handleSubmit(({ password }) =>
+    recoverPasswordMutation.mutate({ password, accessToken, refreshToken })
+  )
+
+  useEffect(() => {
+    if (errorCode) navigate('/login')
+  }, [errorCode])
 
   return (
     <div className='w-xs flex flex-col gap-2.5'>
@@ -47,7 +59,7 @@ export const RecoverPassword: FC = () => {
       <PasswordInput placeholder={t('recover_password.password')} {...register('password')} />
       <PasswordInput placeholder={t('recover_password.confirm_password')} {...register('confirmPassword')} />
       <LoaderButton
-        disabled={!password || !confirmPassword || password !== confirmPassword}
+        disabled={!password || !confirmPassword || password !== confirmPassword || !accessToken || !refreshToken}
         isLoading={recoverPasswordMutation.isPending}
         label={t('recover_password.button')}
         onClick={onSubmit}
