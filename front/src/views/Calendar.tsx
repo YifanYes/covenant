63b7bee99810cf8c1f89bb4701c48ee5debd7616
Yifan <yifanyemontpe@gmail.com>
2025-09-26 12:27:57 +0200

    use suspense

--- front/src/views/Calendar.tsx
+++ front/src/views/Calendar.tsx
@@ -3,14 +3,14 @@ import MonthlyCalendar from '@/components/calendars/MonthlyCalendar'
 import { useCalendarStore } from '@/hooks/use-calendar-store'
 import { getMonth } from '@/utils/calendar.utils'
 import { trpc } from '@/utils/trpc.utils'
-import { useQuery } from '@tanstack/react-query'
-import { Suspense, useEffect, useState } from 'react'
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { useEffect, useState } from 'react'
 
 export default function Calendar() {
   const [currentMonth, setCurrentMonth] = useState(getMonth())
   const { monthIndex } = useCalendarStore()
 
-  const { data: tasksData } = useQuery(
+  const { data: tasksData } = useSuspenseQuery(
     trpc.tasks.getByDate.queryOptions({
       monthIndex: monthIndex.toString(),
       year: currentMonth[0]?.[0]?.year().toString()
@@ -22,15 +22,13 @@ export default function Calendar() {
   }, [monthIndex])
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className='flex h-screen'>
-        <div className='h-full flex-1'>
-          <MonthlyCalendar month={currentMonth} tasks={tasksData?.tasks || []} />
-        </div>
-        <div className='h-full w-60 border-l border-gray-700'>
-          <CalendarSidebar />
-        </div>
+    <div className='flex h-screen'>
+      <div className='h-full flex-1'>
+        <MonthlyCalendar month={currentMonth} tasks={tasksData?.tasks || []} />
       </div>
-    </Suspense>
+      <div className='h-full w-60 border-l border-gray-700'>
+        <CalendarSidebar />
+      </div>
+    </div>
   )
 }
