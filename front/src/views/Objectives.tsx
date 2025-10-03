eeb3fc8d05a7b102083a20f8b8426eda754e1e4b
Yifan <yifanyemontpe@gmail.com>
2025-10-03 17:03:13 +0200

    feat: objectives ui (#15)

--- front/src/views/Objectives.tsx
+++ front/src/views/Objectives.tsx
@@ -1,18 +1,26 @@
 import { CreateAreaDialog } from '@/components/dialogs/CreateAreaDialog'
+import { CreateObjectiveDialog } from '@/components/dialogs/CreateObjectiveDialog'
 import { UpdateAreaDialog } from '@/components/dialogs/UpdateAreaDialog'
+import ObjectiveCard from '@/components/ObjectiveCard'
 import { trpc } from '@/utils/trpc.utils'
 import { useSuspenseQuery } from '@tanstack/react-query'
 import { useTranslation } from 'react-i18next'
 
 export const Objectives = () => {
   const { t } = useTranslation()
+  const { data: objectivesData } = useSuspenseQuery(trpc.objectives.getAll.queryOptions())
   const { data: areasData } = useSuspenseQuery(trpc.areas.getAll.queryOptions())
 
   return (
-    <div className='mx-auto flex max-w-3xl flex-col gap-y-12 p-6'>
+    <div className='flex max-w-3xl flex-col gap-y-12 p-6'>
       <section className='flex flex-col gap-y-6'>
-        <h2 className='text-foreground text-xl font-semibold'>{t('objectives.title')}</h2>
-        <p className='text-foreground'>TO DO: add objectives</p>
+        <div className='flex items-center justify-between'>
+          <h2 className='text-foreground text-xl font-semibold'>{t('objectives.title')}</h2>
+          <CreateObjectiveDialog />
+        </div>
+        {objectivesData.objectives?.map((objective) => (
+          <ObjectiveCard key={objective.id} objective={objective} />
+        ))}
       </section>
       <section className='flex flex-col gap-y-6'>
         <div className='flex items-center justify-between'>
