79f5ee880d371b1ca63b2d5a2f3eea02a818e852
Yifan <yifanyemontpe@gmail.com>
2025-10-03 13:55:48 +0200

    list and create objectives

--- front/src/views/Dashboard.tsx
+++ front/src/views/Dashboard.tsx
@@ -1,20 +1,7 @@
-import ObjectiveCard from '@/components/ObjectiveCard'
-import { trpc } from '@/utils/trpc.utils'
-import { useSuspenseQuery } from '@tanstack/react-query'
-
 export default function Dashboard() {
-  const { data } = useSuspenseQuery(trpc.objectives.getAll.queryOptions())
-
   return (
     <div>
       <h1>Dashboard</h1>
-      {data.objectives?.map((objective) => (
-        <ObjectiveCard
-          key={objective.id}
-          name={objective.name}
-          {...(objective.description && { description: objective.description })}
-        />
-      ))}
     </div>
   )
 }
