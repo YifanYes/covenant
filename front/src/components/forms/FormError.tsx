eb975e815c90f250740677e350d179ab7a76d253
Denis Gudiña Nuñez <syrewolfdigital@gmail.com>
2025-11-21 03:04:36 +0100

    feat(forms): add form error and form field components

---
+++ front/src/components/forms/FormError.tsx
@@ -0,0 +1,9 @@
+export default function FormError({ message }: { message?: string }) {
+  if (!message) return null
+
+  return (
+    <p className='text-destructive text-sm' role='alert'>
+      {message}
+    </p>
+  )
+}
