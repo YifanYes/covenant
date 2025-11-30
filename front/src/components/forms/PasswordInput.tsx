913878fe875156f1ed8ab3a6f6d4a2461fcfcad2
Denis Gudiña Nuñez <syrewolfdigital@gmail.com>
2025-11-30 23:46:57 +0100

    feat(forms): remove forms red placeholder color

--- front/src/components/forms/PasswordInput.tsx
+++ front/src/components/forms/PasswordInput.tsx
@@ -34,8 +34,7 @@ export default function PasswordInput({
           aria-invalid={!!errorMessage}
           className={cn(
             'file:text-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input hover:bg-accent hover:text-accent-foreground dark:hover:bg-input/50 flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-all duration-200 outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-            required ? 'placeholder:text-destructive' : 'placeholder:text-muted-foreground',
-            'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
+            'placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
             'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
             className
           )}
