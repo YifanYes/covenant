c2de77bc3f0193649c696b815ca05a9479699688
Denis Gudiña Nuñez <syrewolfdigital@gmail.com>
2025-12-07 15:22:12 +0100

    fix(dayjs): update dayjs locale on language change

--- front/src/i18n.ts
+++ front/src/i18n.ts
@@ -1,7 +1,13 @@
+import dayjs from 'dayjs'
+import 'dayjs/locale/es'
+import LocalizedFormat from 'dayjs/plugin/localizedFormat'
 import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import Backend from 'i18next-http-backend'
 import { initReactI18next } from 'react-i18next'
+
+dayjs.extend(LocalizedFormat)
+dayjs.locale(i18n.language)
 
 i18n
   .use(Backend)
@@ -14,5 +20,7 @@ i18n
     },
     supportedLngs: ['en', 'es']
   })
+
+i18n.on('languageChanged', (lng) => dayjs.locale(lng))
 
 export default i18n
