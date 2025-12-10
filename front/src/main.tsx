c43ae3fe5684d5d878939aa1326a607a3c11413a
Denis Gudiña Nuñez <58982694+SyreWolf@users.noreply.github.com>
2025-12-10 11:07:36 +0100

    feat: improve habits UX/UI (#37)

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,14 +1,10 @@
 import { QueryClientProvider } from '@tanstack/react-query'
-import dayjs from 'dayjs'
-import LocalizedFormat from 'dayjs/plugin/localizedFormat'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './i18n.ts'
 import './index.css'
 import { Router } from './Router.tsx'
 import { queryClient } from './utils/trpc.utils.ts'
-
-dayjs.extend(LocalizedFormat)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
