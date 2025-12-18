23913646b18dc91cf011646fd3e75e8f0ef84278
Yifan <yifanyemontpe@gmail.com>
2025-12-18 02:42:03 +0100

    build: upgrade to prisma 7 (#42)

--- server/lib/prisma.ts
+++ server/lib/prisma.ts
@@ -1,7 +1,13 @@
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
 import { PrismaClient } from '../generated/prisma/client'
+
+const connectionString = process.env.DATABASE_URL
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
