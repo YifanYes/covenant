c87bc6fdd3c29d9541181616a432b25fa5b5df94
Denis Gudiña Nuñez <58982694+SyreWolf@users.noreply.github.com>
2026-01-05 10:25:35 +0100

    feat: allow objectives completion (#50)

--- shared/schemas/objectives.schemas.ts
+++ shared/schemas/objectives.schemas.ts
@@ -1,17 +1,6 @@
 import { z } from 'zod'
 
-export const createObjectiveSchema = z.object({
-  name: z.string().min(1, 'errors.required_field'),
-  description: z.string().optional(),
-  dueDate: z
-    .date()
-    .nullish()
-    .or(z.string().transform((str) => (str ? new Date(str) : null))),
-  areas: z.array(z.uuid()).optional()
-})
-export type CreateObjectiveBodyType = z.input<typeof createObjectiveSchema>
-
-export const updateObjectiveSchema = z.object({
+export const objectiveSchema = z.object({
   id: z.uuid(),
   name: z.string().min(1, 'errors.required_field'),
   description: z.string().optional(),
@@ -19,9 +8,29 @@ export const updateObjectiveSchema = z.object({
     .date()
     .nullish()
     .or(z.string().transform((str) => (str ? new Date(str) : null))),
-  areas: z.array(z.uuid()).optional()
+  areas: z.array(z.uuid()).optional(),
+  createdAt: z.date().optional(),
+  updatedAt: z.date().optional(),
+  completedAt: z.date().nullish()
+})
+
+export const createObjectiveSchema = objectiveSchema.omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
+  completedAt: true
+})
+export type CreateObjectiveBodyType = z.input<typeof createObjectiveSchema>
+
+export const updateObjectiveSchema = objectiveSchema.omit({
+  createdAt: true,
+  updatedAt: true,
+  completedAt: true
 })
 export type UpdateObjectiveBodyType = z.input<typeof updateObjectiveSchema>
+
+export const completeObjectiveSchema = objectiveSchema.pick({ id: true })
+export type CompleteObjectiveBodyType = z.input<typeof completeObjectiveSchema>
 
 export const deleteObjectiveSchema = z.object({
   id: z.uuid()
