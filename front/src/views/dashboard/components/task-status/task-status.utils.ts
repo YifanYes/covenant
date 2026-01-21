4892047cdbf8fcf88496da43469f39c708ae9014
Denis Gudiña Nuñez <syrewolfdigital@gmail.com>
2026-01-22 00:19:29 +0100

    fix(tasks status): remove done tasks from chart

--- front/src/views/dashboard/components/task-status/task-status.utils.ts
+++ front/src/views/dashboard/components/task-status/task-status.utils.ts
@@ -7,5 +7,5 @@ export const getTaskStatusStats = (stats: Record<string, number>) => ({
     { name: defaultTaskConfig.DONE.label, data: stats.DONE || 0, fill: defaultTaskConfig.DONE.color },
     { name: defaultTaskConfig.OVERDUE.label, data: stats.OVERDUE || 0, fill: defaultTaskConfig.OVERDUE.color }
   ].filter((item) => item.data > 0),
-  totalTasks: (stats.TODO || 0) + (stats.DOING || 0) + (stats.DONE || 0) + (stats.OVERDUE || 0)
+  totalTasks: (stats.TODO || 0) + (stats.DOING || 0) + (stats.OVERDUE || 0)
 })
