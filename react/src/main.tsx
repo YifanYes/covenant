19026ef96a3dae6d87c910bd868b531bcaaec93c
Yifan <yifanyemontpe@gmail.com>
2026-01-25 16:08:09 +0100

    move front to react

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './i18n.ts'
 import './index.css'
-import { Router } from './router'
+import { Router } from './router.tsx'
 import { queryClient } from './utils/trpc.utils.ts'
 
 createRoot(document.getElementById('root')!).render(
